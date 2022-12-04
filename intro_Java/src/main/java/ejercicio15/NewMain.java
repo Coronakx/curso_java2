31d1b173b66e9cf1ad67ee4f5f715a6e5e969fc7
Coronakx <119008910+Coronakx@users.noreply.github.com>
2022-12-04 00:16:00 -0300

    Update NewMain.java

--- intro_Java/src/main/java/ejercicio15/NewMain.java
+++ intro_Java/src/main/java/ejercicio15/NewMain.java
@@ -19,26 +19,27 @@ public class NewMain {
         // TODO code application logic here
         int num1, num2, opcion, res;
         
-
-        
+        System.out.println("Captura dos números para realizar operaciones matemáticas");
         System.out.println("Ingresa el primer número");
         Scanner leer = new Scanner(System.in);
         num1 = leer.nextInt();
         System.out.println("Ingresa el segundo número");
         num2 = leer.nextInt();
         
-
-
-        
-        System.out.println("1. SUMAR");
+        System.out.println("MENÚ");
+        System.out.println("1.SUMAR");
         System.out.println("2.RESTAR");
         System.out.println("3.MULTIPLICAR");
         System.out.println("4.DIVIDIR");
         System.out.println("5.SALIR");
         
+        
         System.out.println("Ingresa una opción");
         opcion = leer.nextInt();
+        if (opcion>5){
+            System.out.println("Opción Inválida");
                
+ 
         
         switch(opcion){
         
