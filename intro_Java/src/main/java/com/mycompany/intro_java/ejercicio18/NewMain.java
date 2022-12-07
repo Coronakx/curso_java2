bdce85dc85ab18f6210d7f86c2cfd3bef8579905
Coronakx <119008910+Coronakx@users.noreply.github.com>
2022-12-07 15:25:26 -0300

    Update NewMain.java

--- intro_Java/src/main/java/com/mycompany/intro_java/ejercicio18/NewMain.java
+++ intro_Java/src/main/java/com/mycompany/intro_java/ejercicio18/NewMain.java
@@ -8,6 +8,7 @@ public class NewMain {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        //Ejercicio 2 Extra
         int num, cont;
         System.out.println("Ingrese una cifra de tres dígitos");
         Scanner leer=new Scanner (System.in);
