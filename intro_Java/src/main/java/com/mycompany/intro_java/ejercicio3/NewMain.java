aa710291eeb9b77397244bfb301ac592f5c8e8a1
Coronakx <119008910+Coronakx@users.noreply.github.com>
2022-12-09 22:16:15 -0300

    Update NewMain.java

--- intro_Java/src/main/java/com/mycompany/intro_java/ejercicio3/NewMain.java
+++ intro_Java/src/main/java/com/mycompany/intro_java/ejercicio3/NewMain.java
@@ -16,12 +16,19 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     // TODO code application logic here
-      String nombre;
-        System.out.print("Ingresa tu nombre: ");
-        Scanner leer = new Scanner(System.in);
-        nombre = leer.nextLine();
-        System.out.println("Tu nombre es: "+nombre);
+       // TODO code application logic here
+       //Ejercicio 3 extra
+       string letra;
+
+        System.out.println("Ingresa una letra");
+        Scanner leer=new Scanner(System.in);
+        letra=leer.nextLine();
+        if (!letra.equals.IgnoreCase("A")||!letra.equals.IgnoreCase("e")||!letra.equals.IgnoreCase("I")||
+       !letra.equals.IgnoreCase("O")||!letra.equals.IgnoreCase("U")) {
+        System.out.println("La letra no es una vocal");
+       } 
+        
+        
     }
     
 }
