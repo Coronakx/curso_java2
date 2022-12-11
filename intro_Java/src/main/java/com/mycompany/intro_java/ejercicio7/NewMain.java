efd685d16d5b9012ef22adfc76ac3a6a184896fe
Coronakx <119008910+Coronakx@users.noreply.github.com>
2022-12-11 15:30:13 -0300

    Update NewMain.java

--- intro_Java/src/main/java/com/mycompany/intro_java/ejercicio7/NewMain.java
+++ intro_Java/src/main/java/com/mycompany/intro_java/ejercicio7/NewMain.java
@@ -16,22 +16,71 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        int num1, num2;
-        System.out.println("Ingrese el primer número:");
-        Scanner leer= new Scanner(System.in);
-        num1=leer.nextInt();
-        System.out.println("Ingrese el segundo número");
-        num2=leer.nextInt();
-        if (num1>num2) {
-            System.out.println("El primer número es el mayor"); 
-        }
-            else {
-                    System.out.println("El segundo número es el mayor");
-                    }    
-   
-        }
+         // TODO code application logic here
+        // Ejercicio 7 extra
+        Scanner leer= new Scanner(System.in);  
        
+        int lim;
+        double num, prom, minVal;
+
+        double maxVal=0;
+        int contador=0;
+        double suma=0;
+
+        //VERSION DO-WHILE
+
+        do {
+        System.out.println("Ingrese un número");
+        num=leer.nextInt();
+        minVal=num;
+        suma=suma+num;
+        if (num>maxval) {
+            maxVal=num;
+            else {
+              if (num<minVal) {
+                minVal=num;
+              }
+
+            }
+        }
+        contador++;
+        }while contador=lim;
+
+        prom=suma/num;
+        System.out.orintln("El número máximo es: "+maxVal);
+        System.out.println("El número mínimo es: "+minVal);
+        System.out.println("El promedio es: "+prom);
+
+
+
+        //VERSION WHILE
+
+        while (contador<=lim) {
+
+                System.out.println("Ingrese un número");
+        num=leer.nextInt();
+        minVal=num;
+        suma=suma+num;
+        if (num>maxval) {
+            maxVal=num;
+            else {
+              if (num<minVal) {
+                minVal=num;
+              }
+
+            }
+        }
+        contador++;
+        }
+        prom=suma/num;
+        System.out.orintln("El número máximo es: "+maxVal);
+        System.out.println("El número mínimo es: "+minVal);
+        System.out.println("El promedio es: "+prom);
+
+
+      }    
+   
+}
         
         
         
