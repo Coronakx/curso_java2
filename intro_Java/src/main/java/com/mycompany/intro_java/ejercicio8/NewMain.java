7e225e58dbcc72a8e9aa963a403d470d0867a14c
Coronakx <119008910+Coronakx@users.noreply.github.com>
2022-12-12 18:41:33 -0300

    Update NewMain.java

--- intro_Java/src/main/java/com/mycompany/intro_java/ejercicio8/NewMain.java
+++ intro_Java/src/main/java/com/mycompany/intro_java/ejercicio8/NewMain.java
@@ -17,17 +17,37 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        double num;
-        System.out.println("Ingrese un número");
-        Scanner leer= new Scanner(System.in);
-        num=leer.nextDouble();
-          
-        if (num%2==0){
-            System.out.println("El número es PAR");
+      //Ejercicio 8 extra
+    Scanner leer = new Scanner(System.in);
+    int lim;
+    double num;
+    int contador=0;
+    int pares=0;
+    int impares=0;
+
+    System.out.println("Ingrese la cantidad de números a capturar");
+    lim=leer.nextInt();
+    while (contador<=lim) {
+        System.out.println("Ingresa un número diferente de cero");
+        num = leer.nextInt();
+        if (num%5==0) {
+            if ((num%2==0)&&(num>0)){
+                pares++
+            }
+                else {
+                    if ((num%2!=0)&&(num>0)) {
+                    impares++
+                    }
+                }
+        contador++;
+        break;
         }
-        else {
-            System.out.println("El número es IMPAR");
-        }
+    }
+    
+    System.out.println("Cantidad de números capturados: "+contador); 
+    System.out.println("Cantidad de números pares: "+pares);
+    System.out.println("Cantidad de números impares: "+impares);  
+
         
         
         
