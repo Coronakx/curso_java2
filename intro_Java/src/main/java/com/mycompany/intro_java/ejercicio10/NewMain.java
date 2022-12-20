326ee258cf90efe473ad5dce3ba04d35bcf41d84
Coronakx <119008910+Coronakx@users.noreply.github.com>
2022-12-20 17:33:59 -0300

    Update NewMain.java

--- intro_Java/src/main/java/com/mycompany/intro_java/ejercicio10/NewMain.java
+++ intro_Java/src/main/java/com/mycompany/intro_java/ejercicio10/NewMain.java
@@ -18,15 +18,35 @@ public class NewMain {
     public static void main(String[] args) {
     // TODO code application logic here
 
-    import java.util.Random; //Esto importa la clase Random de java.util
+    /*import java.util.Random; //Esto importa la clase Random de java.util
 
     publicclassMathMatchRandom { //Esto crea una instancia de la clase Random
         Random claseRandom = new Random();
         claseRandom.nextInt(11); //Genera un número entre 0 y 10
     }   
-}
+}    */
         
-        
+     
+    Scanner leer=new Scanner (System.in);  
+    int numero, aleatorio;
+    int contador=0;
+
+    aleatorio= (int) (Math.random()*100); 
+    
+    do {
+        System.out.println("Adivina el resultado de la multiplicación de dos números de 0-100");
+        numero=leer.nextInt();
+        if (aleatorio>numero) {
+            System.out.println("Digita un número mayor");
+        }
+        else {
+            System.out.println("Digita un número menor");
+        }
+        contador++;
+
+    }while (numero !=aleatorio);
+    System.out.println("¡¡¡¡Adivinaste el resultado!!!!!!! en " +contador+"intentos");
+          
         
         
         
