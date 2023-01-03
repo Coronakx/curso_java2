6d9eec80445fa9fc5efcbab390e87a471879a102
Coronakx <119008910+Coronakx@users.noreply.github.com>
2023-01-03 19:06:25 -0300

    Update NewMain.java

--- intro_Java/src/main/java/com/mycompany/intro_java/ejercicio22/NewMain.java
+++ intro_Java/src/main/java/com/mycompany/intro_java/ejercicio22/NewMain.java
@@ -15,6 +15,30 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+                //Ejercicio 22 
+    
+        Scanner leer=new Scanner (System.in);
+        int num=1;
+        int[]vector = new int[100];
+    
+    //Llenar el vector de 1 a 100    
+        for (int i=0; i<vector.length; i++) {
+            vector[i]=num;
+            num=num+1;
+        
+      }
+    //Mostrar el arreglo de forma descendente
+        for (int i=vector.length-1; i>=0; i--) {
+            System.out.print(""+vector[i]);
+     
+            }
+        }
+        
+        
+        
+        
+        
     }
+   
     
 }
