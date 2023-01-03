fe3f5d23acc6575fea25618dc00d6c557b3b7f60
Coronakx <119008910+Coronakx@users.noreply.github.com>
2023-01-03 19:39:53 -0300

    Update NewMain.java

--- intro_Java/src/main/java/com/mycompany/intro_java/ejercicio23/NewMain.java
+++ intro_Java/src/main/java/com/mycompany/intro_java/ejercicio23/NewMain.java
@@ -15,6 +15,44 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+                //Ejercicio 23 
+        Scanner leer=new Scanner (System.in);
+        int n, busca;
+
+        System.out.println("Ingrese la dimensión del vector");
+        n=leer.nextInt();
+        int[]vector = new int [n];
+
+        //Llenar el vector con números aleatorios
+  
+        for (int i=0; i<vector.length; i++) {
+            int aleatorio = (int) (Math.random()*100+1);
+            vector[i]=aleatorio;
+        }
+
+        for (int i=0; i<vector.length;i++) {
+            System.out.println(" "+vector[i]);
+        }
+        //Buscar un número en el vector
+
+        System.out.println("Ingresa el número que quieres encontrar en el vector");
+        busca=leer.nextInt();
+
+        
+        for (int i=0; i<vector.length;i++) {
+            if (busca=vector[i]) {
+            System.out.println("El valor  "+busca "está en la posición "+vector[i]);
+            }
+            else {
+            System.out.println("El valor "+busca "No está en el vector");
+    
+            }
+        }
+        
+        
+        
+        
+        
     }
     
 }
