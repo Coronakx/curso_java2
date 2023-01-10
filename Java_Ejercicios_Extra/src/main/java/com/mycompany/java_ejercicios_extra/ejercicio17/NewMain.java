2b56c3745104023d5ddabe7121641e2f5a286a86
Coronakx <119008910+Coronakx@users.noreply.github.com>
2023-01-10 18:55:01 -0300

    Update NewMain.java

--- Java_Ejercicios_Extra/src/main/java/com/mycompany/java_ejercicios_extra/ejercicio17/NewMain.java
+++ Java_Ejercicios_Extra/src/main/java/com/mycompany/java_ejercicios_extra/ejercicio17/NewMain.java
@@ -15,6 +15,31 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner leer=new Scanner (System.in);
+        int n;
+        int suma=0;
+
+        System.out.println("Ingrese la dimensión del vector");
+        n=leer.nextInt();
+        int[]vector = new int [n];
+
+        //Llenar el vector 
+  
+        for (int i=0; i<vector.length; i++) {
+            System.out.println("Ingrese el valor del vector en la posición "+i+" ");
+            vector[i]=leer.nextInt();
+        }
+        //Mostrar el vector
+        for (int i=0; i<vector.length;i++) {
+            System.out.println(" "+vector[i]);
+        } 
+
+        //Sumar los elementos del vector
+        for (int i=0; i<vector.length;i++) {
+            suma=suma+vector[i];  
+            System.out.println("La suma de los elementos del vector es: "+suma);
+        } 
+        
     }
-    
+  
 }
