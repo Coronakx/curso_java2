56dab0681f5fa9d97919e3e93a6c74952b752cb9
Coronakx <119008910+Coronakx@users.noreply.github.com>
2023-01-10 17:43:26 -0300

    Update NewMain.java

--- Java_Ejercicios_Extra/src/main/java/com/mycompany/java_ejercicios_extra/ejercicio16/NewMain.java
+++ Java_Ejercicios_Extra/src/main/java/com/mycompany/java_ejercicios_extra/ejercicio16/NewMain.java
@@ -15,6 +15,32 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+             // Ejercicio 16 extra
+        Scanner leer=new Scanner (System.in);  
+        int num;
+        boolean primo=true;
+
+    
+        System.out.println("Ingresa un número");
+        num=leer.nextInt();
+        numPrimo(int num);
+
+    }
+
+    public void numPrimo(int num) {
+        for (int i=2; i<num && primo==true; i++) {
+        if (num%i==0) {
+           primo=false;
+        }   
+        }
+        if (primo=true) {
+            System.out.println("El número es primo");
+        } else {
+            System.out.println("El número NO es primo");
+        }
+    
     }
     
-}
+}        
+        
+ 
