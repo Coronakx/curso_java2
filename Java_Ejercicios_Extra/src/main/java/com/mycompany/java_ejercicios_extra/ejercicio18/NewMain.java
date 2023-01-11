36bb3e9774b3aa7166039fcbe814a861c2602746
Coronakx <119008910+Coronakx@users.noreply.github.com>
2023-01-11 19:49:13 -0300

    Update NewMain.java

--- Java_Ejercicios_Extra/src/main/java/com/mycompany/java_ejercicios_extra/ejercicio18/NewMain.java
+++ Java_Ejercicios_Extra/src/main/java/com/mycompany/java_ejercicios_extra/ejercicio18/NewMain.java
@@ -15,6 +15,46 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+                Scanner leer=new Scanner (System.in);
+        int n;
+        
+
+        System.out.println("Ingrese la dimensión de los vectores");
+        n=leer.nextInt();
+        int[]vector1 = new int [n];
+        int[]vector2 = new int [n]; 
+
+
+        //Llenar el vector 1 
+        for (int i=0; i<vector1.length; i++) {
+            System.out.println("Ingrese el valor del vector 1 en la posición "+i+" ");
+            vector1[i]=leer.nextInt();
+       
+        //Llenar el vector 2
+        for (int i=0; i<vector2.length; i++) {
+            System.out.println("Ingrese el valor del vector 2 en la posición "+i+" ");
+            vector2[i]=leer.nextInt();
+        
+        //Mostrar los vectores 
+        
+        for (int i=0; i<n; i++) {
+            System.out.println(vector1[i]+" ");
+            System.out.println(vector2[i]+" ");
+        }
+        
+
+        //Comparar los vectores
+        for (int i=0; i<vector1.length; i++) {
+            if vector1[i]!==vector2[i] {
+                System.out.println("Los elementos de los vectores 1 y 2 son diferentes");
+                break;
+            } else {
+                System.out.println("Los elementos de los vectores 1 y 2 son iguales");
+            }
+            
+        }
+        
+        
     }
     
 }
