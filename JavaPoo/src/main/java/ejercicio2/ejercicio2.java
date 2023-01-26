405877dea45d19d1b8fe092cd2146b175f179aed
Coronakx <119008910+Coronakx@users.noreply.github.com>
2023-01-26 09:24:20 -0300

    Update ejercicio2.java

--- JavaPoo/src/main/java/ejercicio2/ejercicio2.java
+++ JavaPoo/src/main/java/ejercicio2/ejercicio2.java
@@ -20,5 +20,28 @@ public class ejercicio2 {
         circunferencia.MetArea();
         circunferencia.MetPerim();
     }
+  /*
+    import java.util.Arrays;
+    import java.util.*;
+    
+    double vec1=new double[50];
+    double vec2=new double[20];
+    int i;
+    
+    for (i=0; i<vec1.lenght;i++) {
+        
+        Arrays.sort(vec1);
+        System.out.porintln(Arrays.toString(vec1));
+        */
+        
+    import java.time.LocalDate;
+    
+    LocalDate fecha1= LocalDate.now; //fecha actual
+    LocalDate fecha2= LocalDate.parse("2023-01-10);
+                                      
+    fecha2=fecha2.plusDays(8);
+    fecha2=fecha2.plusMonths(5);
+    fecha2=fecha2.plusYears(3);
+    fecha2=fecha2.minusDays(4);                                   
     
 }
